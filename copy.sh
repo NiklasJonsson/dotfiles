8ffb8471c05a412476b83cdd150cb69d27b6c30d
Niklas Jonsson <niklasandersjonsson@gmail.com>
2018-12-22 10:38:41 +0100

    only use symlinks

--- copy.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-echo "copying from home ..."
-cp ../.bash_aliases .
-cp ../.bashrc .
-cp -r ../.vim* .
-cp ../.gitconfig .
-rm .viminfo
-echo "...done"
--- paste.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-echo "Copying to home..."
-cp .bash_aliases ../
-cp .bashrc ../
-cp -r .vim* ../
-cp .gitconfig ../
-echo "...done"
